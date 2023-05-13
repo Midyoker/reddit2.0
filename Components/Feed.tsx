b9de3c992fa1270de0912dc871b907a2bd401e14
Mohit <mohitsharma20020720@gmail.com>
2023-05-14 04:03:55 +0530

    added changes

--- Components/Feed.tsx
+++ Components/Feed.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import { useQuery } from '@apollo/client'
-import { GET_ALL_POSTS } from '../graphql/queries'
-import Post from './Post'
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_POSTS, GET_ALL_POSTS_BY_TOPIC } from '../graphql/queries';
+import Post from './Post';
 
-function Feed() {
-  const { data, error } = useQuery(GET_ALL_POSTS)
-  const posts: Post[] = data?.getAllPosts;
+type Props = {
+  topic?: string;
+};
+
+function Feed({ topic }: Props) {
+  const { data, error } = !topic
+    ? useQuery(GET_ALL_POSTS)
+    : useQuery(GET_ALL_POSTS_BY_TOPIC, {
+        variables: {
+          topic: topic,
+        },
+      });
+  const posts: Post[] = !topic ? data?.getPostList: data?.getPostListByTopic;
 
   return (
-    <div className='mt-5 space-y-4'>
-
+    <div className="mt-5 space-y-4">
       {posts?.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </div>
-  )
-   }
+  );
+}
 
-export default Feed
+export default Feed;
--- Components/PostBox.tsx
+++ Components/PostBox.tsx
@@ -6,10 +6,8 @@ import { useForm } from 'react-hook-form';
 import { useMutation } from '@apollo/client';
 import { ADD_POST, ADD_SUBREDDIT } from '../graphql/mutations';
 import client from '../apollo-client';
-import { GET_SUBREDDIT_BY_TOPIC } from '../graphql/queries';
+import { GET_SUBREDDIT_BY_TOPIC, GET_ALL_POSTS } from '../graphql/queries';
 import { toast } from 'react-hot-toast';
-import { GET_ALL_POSTS } from '../graphql/queries';
-import { type } from 'os';
 
 type FormData = {
   postTitle: string;
@@ -20,14 +18,14 @@ type FormData = {
 
 type Props = {
   subreddit?: string;
-}
+};
 
 function PostBox({ subreddit }: Props) {
   const { data: session } = useSession();
-  const [addPost] = useMutation(ADD_POST ,{
-    refetchQueries: [GET_ALL_POSTS,'getPostlist'],
-  })
-  const [addSubreddit] = useMutation(ADD_SUBREDDIT)
+  const [addPost] = useMutation(ADD_POST, {
+    refetchQueries: [{ query: GET_ALL_POSTS }],
+  });
+  const [addSubreddit] = useMutation(ADD_SUBREDDIT);
 
   const [imageBoxOpen, setImageBoxOpen] = useState(false);
   const {
@@ -36,85 +34,76 @@ function PostBox({ subreddit }: Props) {
     watch,
     formState: { errors },
     setValue,
-
   } = useForm<FormData>();
 
   const onSubmit = handleSubmit(async (formData) => {
     console.log(formData);
-    const notification =toast.loading('Creating  new post...')
+    const notification = toast.loading('Creating new post...');
 
     try {
-      //subreddit query
       const {
-        data: { getSubredditListByTopic },
+        data: { getSubredditByTopic },
       } = await client.query({
         query: GET_SUBREDDIT_BY_TOPIC,
         variables: {
           topic: subreddit || formData.subreddit,
         },
       });
-      const subredditExists = getSubredditListByTopic.length > 0;
+      const subredditExists = getSubredditByTopic.length > 0;
       if (!subredditExists) {
-        console.log("Subreddit is new -> creating new subreddit")
+        console.log('Subreddit is new -> creating new subreddit');
 
-        const { data: { insertSubreddit: newSubreddit } } = await addSubreddit({
+        const { data: newSubreddit } = await addSubreddit({
           variables: {
             topic: formData.subreddit,
           },
         });
 
-        console.log("creating post.", formData)
-        const image = formData.postImage || ''
-        const { data: { insertPost: newPost }
-        } = await addPost({
+        console.log('creating post.', formData);
+        const image = formData.postImage || '';
+        const { data: newPost } = await addPost({
           variables: {
             body: formData.postBody,
             image: image,
-            subreddit_id: newSubreddit.id,
+            subreddit_id: newSubreddit.insert_subreddit_one.id,
             title: formData.postTitle,
             username: session?.user?.name,
           },
         });
-        console.log("new post", newPost)
+        console.log('new post', newPost);
+      } else {
+        console.log('using existing subreddit');
+        console.log('getSubredditByTopic', getSubredditByTopic);
 
-      }
-      else {
-        //use existing subreddit
-        console.log("using existing subreddit")
-        console.log("getSubredditListByTopic")
+        const image = formData.postImage || '';
 
-        const image = formData.postImage || ''
-
-        const { data: { insertPost: newPost }, } = await addPost({
+        const { data: newPost } = await addPost({
           variables: {
             body: formData.postBody,
             image: image,
-            subreddit_id: getSubredditListByTopic[0].id,
+            subreddit_id: getSubredditByTopic[0].id,
             title: formData.postTitle,
             username: session?.user?.name,
           },
         });
-        console.log("new post added:", newPost)
+        console.log('new post added:', newPost);
       }
 
-      //After the post has been added
-      setValue('postBody', '')
-      setValue('postTitle', '')
-      setValue('postImage', '')
-      setValue('subreddit', '')
+      setValue('postBody', '');
+      setValue('postTitle', '');
+      setValue('postImage', '');
+      setValue('subreddit', '');
 
-      toast.success('Post created!', { 
+      toast.success('Post created!', {
         id: notification,
-       })
-
+      });
     } catch (error) {
-      console.log(error)
-      toast.error('Whoops!! something went wrong', { 
+      console.log(error);
+      toast.error('Whoops!! something went wrong', {
         id: notification,
-       })
+      });
     }
   });
-
   return (
     <form className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2" onSubmit={onSubmit}>
       <div className="flex items-center space-x-3">
@@ -124,7 +113,7 @@ function PostBox({ subreddit }: Props) {
           disabled={!session}
           className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
           type="text"
-          placeholder={session ? `Create a post in r/${subreddit}`:'Create a post by entering the title! ' : 'Sign in to post'}
+          placeholder={session ? `Create a post in r/${subreddit}` : 'Create a post by entering the title! '}
         />
         <PhotographIcon
           onClick={() => setImageBoxOpen(!imageBoxOpen)}
@@ -132,7 +121,7 @@ function PostBox({ subreddit }: Props) {
         />
         <LinkIcon className="h-6 text-gray-500" />
       </div>
-
+  
       {watch('postTitle') && (
         <div className="flex flex-col py-2">
           <div className="flex items-center px-2">
@@ -144,15 +133,18 @@ function PostBox({ subreddit }: Props) {
               placeholder="Text (optional)"
             />
           </div>
-          <div className="flex items-center px-2">
-            <p className="min-w-[90px]">Subreddit</p>
-            <input
-              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
-              {...register('subreddit', { required: true })}
-              type="text"
-              placeholder="i.e r/MohMaya"
-            />
-          </div>
+          {!subreddit && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Subreddit</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register('subreddit', { required: true })}
+                type="text"
+                placeholder="i.e r/MohMaya"
+              />
+            </div>
+          )}
+  
           {imageBoxOpen && (
             <div className="flex items-center px-2">
               <p className="min-w-[90px]">Image URL</p>
@@ -170,7 +162,7 @@ function PostBox({ subreddit }: Props) {
               {errors.subreddit?.type === 'required' && <p>Subreddit is required</p>}
             </div>
           )}
-
+  
           {!!watch('postTitle') && (
             <button type="submit" className="w-full h-10 rounded-full bg-blue-400 text-white">
               Create post
@@ -180,6 +172,7 @@ function PostBox({ subreddit }: Props) {
       )}
     </form>
   );
+  
 }
 
 export default PostBox;
