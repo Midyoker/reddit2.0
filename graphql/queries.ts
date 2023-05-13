25550758138d1811b6c4870dbb20158d37cbbbbc
Mohit <mohitsharma20020720@gmail.com>
2023-05-13 23:44:29 +0530

    added changes

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,5 +1,19 @@
 import { gql } from '@apollo/client';
 
+export const GET_ALL_POSTS = gql`
+    query MyQuery {
+        getPostList {
+            body,
+            id,
+            title,
+            created_at,
+            image,
+            username,
+            subreddit_id,
+        }
+    }
+`;
+
 export const GET_SUBREDDIT_BY_TOPIC = gql`
     query MyQuery($topic: String!) {
         getSubredditByTopic(topic: $topic) {
