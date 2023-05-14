e44ba9a5cbab3b4d3659b1d5cd9a8f50551e777f
Mohit <mohitsharma20020720@gmail.com>
2023-05-14 16:35:58 +0530

    added

--- Components/Post.tsx
+++ Components/Post.tsx
@@ -1,48 +1,109 @@
 import {
-    ArrowDownIcon,
-    ArrowUpIcon,
-    ChatAltIcon,
-    ShareIcon,
-    ThumbUpIcon,
-    ThumbDownIcon,
-    DotsHorizontalIcon,
-    ExclamationIcon,
-    FlagIcon,
-    BookmarkIcon,
-    GiftIcon,
-  } from '@heroicons/react/solid';
-  import React from 'react';
-  import Avatar from './Avatar';
-  import TimeAgo from 'react-timeago';
-  import Link from 'next/link';
-  import { Jelly } from '@uiball/loaders';
-  
-  type Props = {
-    post: Post
-  };
-  
-  function Post({ post }: Props) {
-if(!post) return(
-  <div className='flex w-full items-center justify-center p-10 text-xl'>
-    <Jelly size={50} color="#FF4501"  />
-  </div>
-)
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ChatAltIcon,
+  ShareIcon,
+  ThumbUpIcon,
+  ThumbDownIcon,
+  DotsHorizontalIcon,
+  ExclamationIcon,
+  FlagIcon,
+  BookmarkIcon,
+  GiftIcon,
+} from '@heroicons/react/solid';
+import React, { useEffect } from 'react';
+import Avatar from './Avatar';
+import TimeAgo from 'react-timeago';
+import Link from 'next/link';
+import { Jelly } from '@uiball/loaders';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { GET_ALL_VOTES_BY_POST_ID } from '../graphql/queries';
+import { useQuery } from '@apollo/client';
+import { ADD_VOTE } from '../graphql/mutations';
+import { useMutation } from '@apollo/client';
+import { toast } from 'react-hot-toast';
+import { Vote } from '../types';
 
-    return (
-      <Link href={`/post/${post.id}`}>
+interface Post {
+  id: string;
+  subreddit: {
+    topic: string;
+  }[];
+  username: string;
+  created_at: string;
+  title: string;
+  body: string;
+  image: string;
+  comments: {
+    length: number;
+  }[];
+}
+
+
+type Props = {
+  post: Post
+};
+
+function Post({ post }: Props) {
+  const [vote, setVote] = useState<boolean>();
+  const { data: session } = useSession();
+  const {data, loading} =useQuery(GET_ALL_VOTES_BY_POST_ID, {
+    variables: {
+      postId: post?.id,
+    }
+      }); 
+   const [addVote] = useMutation(ADD_VOTE, {
+    refetchQueries:[{ query: GET_ALL_VOTES_BY_POST_ID }],
+   });   
+
+
+  const upVote = async(isUpvote: boolean) => {
+  if(!session) {
+    toast("! Yuo'll need to login to vote")
+    return;
+  }
+  if(vote === isUpvote) return;
+  if (vote === false && !isUpvote) return;
+
+  await addVote({
+    variables: {
+      postId: post.id,
+      username: session?.user?.name,
+      upvote: isUpvote,
+    },
+  });
+  }
+
+  useEffect(() => {
+    const votes: Vote[] | undefined = data?.getVotesByPostId;
+    const vote = votes?.find((vote) => vote.username === session?.user?.name)?.isUpvote;
+    setVote(vote);
+  
+  }, [data])
+
+
+  if (!post) return (
+    <div className='flex w-full items-center justify-center p-10 text-xl'>
+      <Jelly size={50} color="#FF4501" />
+    </div>
+  )
+
+  return (
+    <Link href={`/post/${post.id}`}>
       <div className="flex cursor-pointer border border-gray-300 bg-white shadow-sm hover:border hover:border-gray-600">
         <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
-          <ArrowUpIcon className="voteButtons hover:text-red-400" />
+          <ArrowUpIcon onClick={() => upVote(true)} className={`voteButtons hover:text-red-400 ${vote && 'text-red-400'}`} />
           <p className="text-xs font-bold text-black">1</p>
-          <ArrowDownIcon className="voteButtons hover:text-blue-400" />
+          <ArrowDownIcon onClick={() => upVote(false)}  className={`voteButtons hover:text-blue-400 ${vote == false && 'text-blue-400'} `} />
         </div>
         <div className="p-3 pb-1">
           <div className="flex items-center space-x-2">
             <Avatar seed={post.subreddit[0].topic} />
             <p className="text-xs text-gray-400">
               <Link href={`/subreddit/${post.subreddit[0]?.topic}`}>
-              <span className="font-bold text-black hover:text-blue-400 hover:underline">r/{post.subreddit[0]?.topic}</span>{' '}
-              . Posted by u/{post.username} <TimeAgo date={post.created_at} />
+                <span className="font-bold text-black hover:text-blue-400 hover:underline">r/{post.subreddit[0]?.topic}</span>{' '}
+                . Posted by u/{post.username} <TimeAgo date={post.created_at} />
               </Link>
             </p>
           </div>
@@ -75,9 +136,8 @@ if(!post) return(
           </div>
         </div>
       </div>
-      </Link>
-    );
-  }
-  
-  export default Post;
-  
+    </Link>
+  );
+}
+
+export default Post;
--- pages/post/[postid].tsx
+++ pages/post/[postid].tsx
@@ -4,10 +4,25 @@ import { useQuery } from '@apollo/client';
 import { GET_POST_BY_POST_ID } from '../../graphql/queries';
 import Post from '../../Components/Post';
 import { useSession } from 'next-auth/react';
+import { SubmitHandler, set, useForm } from 'react-hook-form';
+import { type } from 'os';
+import { useMutation } from '@apollo/client';
+import { ADD_COMMENT } from '../../graphql/mutations';
+import { toast } from 'react-hot-toast';
+import Avatar from '../../Components/Avatar';
+import ReactTimeago from 'react-timeago';
+
+type FormData = {
+    comment: string;
+}
 
 function PostPage() {
     const router = useRouter()
-    const {data: session} = useSession()
+    const { data: session } = useSession()
+    const [addComment] = useMutation(ADD_COMMENT, {
+        refetchQueries: [GET_POST_BY_POST_ID, 'getPostListByPostId'],
+    });
+
     const { data } = useQuery(GET_POST_BY_POST_ID, {
         variables: {
             postId: router.query.postId,
@@ -15,19 +30,74 @@ function PostPage() {
     });
 
     const post: Post = data?.getPostListByPostId;
-    
-  return (
-    <div className='mx-auto my-7 max-w-5xl'>
-        <Post post={post} />
 
-        <div className='-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16'>
-            <p className='text-sm'>
-                Comment as <span>{session?.user?.name}</span>
-            </p>
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+        setValue,
+    } = useForm<FormData>();
+
+    const onsubmit: SubmitHandler<FormData> = async (data) => {
+        //post comment here..
+        console.log(data)
+        const notification = toast.loading('posting your comment...')
+        await addComment({
+            variables: {
+                postId: router.query.postId,
+                username: session?.user?.name,
+                text: data.comment,
+            },
+        });
+
+        setValue('comment', '');
+        toast.success('Comment posted Successfully! ', {
+            id: notification
+        });
+
+    };
+
+    return (
+        <div className='mx-auto my-7 max-w-5xl'>
+            <Post post={post} />
+
+            <div className='-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16'>
+                <p className='text-sm'>
+                    Comment as <span className='text-red-500'>{session?.user?.name}</span>
+                </p>
+                <form onSubmit={handleSubmit(onsubmit)} className='flex  flex-col space-y-2'>
+                    <textarea
+                        {...register('comment')}
+                        disabled={!session}
+                        className='h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50'
+                        placeholder={session ? 'what are your thoghts' : 'Please sign in to comment'} />
+
+                    <button type='submit' className='rounded-full bg-red-500 font-semibold text-white disabled:bg-gray-200'>Comment</button>
+                </form>
+            </div>
+            <div className='-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10'>
+                <hr className='py-2' />
+                {post?.comments?.map((comment) => (
+                    <div className='relative flex items-center space-x-2 space-y-5' key={comment.id}>
+                <hr className='absolute top-10 left-7 h-16 z-0 border ' />
+                        <div>
+                            <Avatar seed={comment.username} />
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className='py-2 text-xs text-gray-400'>
+                                <span className='font-semibold text-gray-600'>{comment.username}</span>
+                                {''}
+                                <ReactTimeago date={comment.created_at} />
+                            </p>
+                            <p>{comment.text}</p>
+                        </div>
+                     </div>   
+                ))}
+            </div>
+
         </div>
-      
-    </div>
-  )
-}
 
+    )
+}
 export default PostPage
